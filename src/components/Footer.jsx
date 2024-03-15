299346e2992c610ad2c4e94867bf847dede59c1a
JeffDanielle <bayotjeff@gmail.com>
2024-03-15 13:17:38 +0800

    39. JavaScript Logic in Components -4mins

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,16 @@ const Footer = () => {
     const hr = time.getHours()
     const min = time.getMinutes()
     const sec = time.getSeconds()
+    const openHr = 8
+    const closeHr = 22
     const timeNow = `${hr}:${min}:${sec}`
+    const isOpen = hr >= openHr && hr <= closeHr
+
+    // if (hr >= openHr && hr <= closeHr) {
+    //     alert("We're currently open!")
+    // } else {
+    //     alert("We're currently closed!")
+    // }
     return (
         <footer className="text-xl text-center mt-10 font-bold">{timeNow}. We're currently open!</footer>
     );
