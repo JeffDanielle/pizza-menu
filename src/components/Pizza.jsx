8bb1733769aa2ce56ad6a56e2f8e0be778aaf402
JeffDanielle <bayotjeff@gmail.com>
2024-03-16 11:41:52 +0800

    53. Setting Classes and Text Conditionally -8min

--- src/components/Pizza.jsx
+++ src/components/Pizza.jsx
@@ -4,12 +4,12 @@ const pizzaComponent = ({ pizzaObj }) => {
 
     return (
         <>
-            <li className="pizza">
+            <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
                 <img src={pizzaObj.photoName} alt="pizza image" />
                 <div>
                     <h3 className="text-start font-bold">{pizzaObj.name}</h3>
                     <p className="text-start">{pizzaObj.ingredients}</p>
-                    <span>{pizzaObj.price + 3}</span>
+                    <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
                 </div>
             </li>
         </>
